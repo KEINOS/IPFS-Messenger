ac91211506bc416fc70ce503a443f5b87d724e96
KEINOS <github@keinos.com>
2021-11-22 04:26:05 +0900

    initial working script (need refactor)

--- go.mod
+++ go.mod
@@ -4,6 +4,13 @@ go 1.16
 
 require (
 	github.com/KEINOS/go-utiles v1.2.1
+	github.com/gdamore/tcell/v2 v2.1.0
 	github.com/kami-zh/go-capturer v0.0.0-20171211120116-e492ea43421d
+	github.com/libp2p/go-libp2p v0.16.0-dev.0.20211121122131-cac4cf3aa159
+	github.com/libp2p/go-libp2p-core v0.11.0
+	github.com/libp2p/go-libp2p-pubsub v0.6.0
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/rivo/tview v0.0.0-20210125085121-dbc1f32bb1d0
 	github.com/stretchr/testify v1.7.0
 )
--- main_test.go
+++ main_test.go
@@ -1,16 +1,66 @@
 package main
 
 import (
+	"os"
 	"testing"
 
+	"github.com/KEINOS/go-utiles/util"
 	"github.com/kami-zh/go-capturer"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_main(t *testing.T) {
+	// Mock os.Args
+	oldOsArgs := os.Args
+	defer func() {
+		os.Args = oldOsArgs
+	}()
+
+	os.Args = []string{
+		t.Name(),
+		"-nick", "foo",
+		"-room", "bar",
+	}
+
+	// Mock os.Exit of util.OsExit
+	oldOsExit := util.OsExit
+	defer func() {
+		util.OsExit = oldOsExit
+	}()
+
+	var exitCode int
+
+	util.OsExit = func(code int) {
+		exitCode = code
+	}
+
+	// Mock os.Stdin
+	oldOsStdin := os.Stdin
+	defer func() {
+		os.Stdin = oldOsStdin
+	}()
+
+	input := []byte("/quit")
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = w.Write(input)
+	if err != nil {
+		t.Error(err)
+	}
+	w.Close()
+
+	// Run main
 	out := capturer.CaptureStdout(func() {
+		os.Stdin = r
+
 		main()
 	})
 
-	assert.Equal(t, out, "Hello, Gopher!\n")
+	// Assertions
+	require.Equal(t, 1, exitCode, "")
+	assert.Empty(t, out)
 }
